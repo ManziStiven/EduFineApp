2ef6a3858d7422d721a00fa7536a6cbc03c27b2d
ManziStiven <stivenmanzi8@gmail.com>
2026-03-11 21:13:58 +0200

    step 1 lab 2 completed

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from core.models import Testing
+from core.models import Testing,Transaction
 admin.site.register(Testing)
+admin.site.register(Transaction)
