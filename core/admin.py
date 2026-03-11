c4396ba999147a6c70fae1064628fef47d6deadb
ManziStiven <stivenmanzi8@gmail.com>
2026-03-11 19:39:08 +0200

    completed lab1

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
 # Register your models here.
+from core.models import Testing
+admin.site.register(Testing)
--- core/views.py
+++ core/views.py
@@ -1,3 +1,20 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from core.models import Testing
+from core.serializers import TestingSerializer
 
-# Create your views here.
+def testing_view(request):
+    testings = Testing.objects.all()
+    serializer = TestingSerializer(testings, many=True)
+    return JsonResponse(serializer.data, safe=False)
+
+def health_check(request):
+    return JsonResponse({'status':'ok'})
+
+def testing_detail_view(request, id):
+    try:
+        testing = Testing.objects.get(id=id)
+        serializer = TestingSerializer(testing)
+        return JsonResponse(serializer.data)
+    except Testing.DoesNotExist:
+        return JsonResponse({'error': 'Record not found'}, status=404)
